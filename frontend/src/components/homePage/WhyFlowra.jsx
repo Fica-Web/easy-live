198423a3acb46c80c2e9838d7974588b69b82ce1
Muhammed Ajmal <ajuajmal1722001@gmail.com>
2025-01-18 12:02:44 +0530

    reusable whyFlowra component

--- frontend/src/components/homePage/WhyFlowra.jsx
+++ frontend/src/components/homePage/WhyFlowra.jsx
@@ -3,24 +3,23 @@ import SpecificationListing from './SpecificationListing';
 
 const arr = [1, 3, 4]
 
-const WhyFlowra = () => {
+const WhyFlowra = ({ title, description, ListingComponent, specifications }) => {
     return (
         <div className='px-20 my-20'>
             <div className='flex flex-col items-center gap-6'>
-                <h2 className='text-primary text-4xl font-semiBold'>
-                    Why Choose Flowra
+                <h2 className='text-primary text-5xl font-semiBold max-w-[600px] text-center'>
+                    { title }
                 </h2>
                 <p className='text-center text-xl text-gray-700 lg:max-w-5xl tracking-wide leading-9'>
-                    Periods deserve a glow up gentle on your body, great for the planet, and drama free. Flowra isn’t just a cup;
-                    it's your new best period buddy. Let's ditch the drama and flow better together
+                    { description }
                 </p>
             </div>
 
-            <SpecificationListing 
-                specifications={arr}
-            />
+            {ListingComponent && (
+                <ListingComponent specifications={specifications} />
+            )}
         </div>
     )
 }
 
-export default WhyFlowra
+export default WhyFlowra;
--- frontend/src/pages/HomePage.jsx
+++ frontend/src/pages/HomePage.jsx
@@ -1,16 +1,28 @@
 import React from 'react';
 import Hero from '../components/homePage/Hero';
 import WhyFlowra from '../components/homePage/WhyFlowra';
+import SpecificationListing from '../components/homePage/SpecificationListing';
 // import CurvedText from '../components/homePage/CurvedText';
 import SustainablePractice from '../components/homePage/SustainablePractice';
 
 const HomePage = () => {
+    const specifications = [1, 3, 2];
+
     return (
         <>
             <Hero />
-            <WhyFlowra />
+            <WhyFlowra 
+                title='Why Choose Flowra'
+                description="Periods deserve a glow up gentle on your body, great for the planet, and drama free. Flowra isn’t just a cup; it's your new best period buddy. Let's ditch the drama and flow better together"
+                ListingComponent={SpecificationListing}
+                specifications={specifications}
+            />
             {/* <CurvedText /> */}
             <SustainablePractice />
+            <WhyFlowra 
+                title='Choose the right Flowra product made just for you'
+                description="At Flowra, we design our products to be your body’s best friend—always gentle, supportive, and reliable. Choosing the right product is always on your hands"
+            />
         </>
     )
 }
