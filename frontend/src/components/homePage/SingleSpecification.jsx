4702595d5cd64676df2234a4c0111280d60e4cb6
Muhammed Ajmal <ajuajmal1722001@gmail.com>
2025-01-21 10:05:19 +0530

    responsive specificationListing

--- frontend/src/components/homePage/SingleSpecification.jsx
+++ frontend/src/components/homePage/SingleSpecification.jsx
@@ -2,12 +2,12 @@ import React from 'react'
 
 const SingleSpecification = ({ specification }) => {
     return (
-        <div className='lg:px-16 px-10 lg:border-r border-gray-700 last:border-r-0'>
+        <div className='w-11/12 mx-auto sm:px-4 lg:px-7 lg:border-r border-gray-700 shadow-md lg:shadow-none mt-5 last:border-r-0 py-8 lg:py-0'>
             <div className='flex flex-col items-center gap-5 py-4'>
-                <div className='lg:border border-gray-700 w-28 h-28 rounded-full'>
+                <div className='border border-gray-700 w-28 h-28 rounded-full'>
 
                 </div>
-                <h3 className='text-primary font-semiBold text-2xl mt-5'>
+                <h3 className='text-primary font-semiBold text-2xl mt-8 lg:mt-1'>
                     Simple and Hassle Free
                 </h3>
                 <p className='text-center text-lg text-gray-700 lg:max-w-72 '>
--- frontend/src/components/homePage/WhyFlowra.jsx
+++ frontend/src/components/homePage/WhyFlowra.jsx
@@ -1,13 +1,10 @@
 import React from 'react';
-import SpecificationListing from './SpecificationListing';
-
-const arr = [1, 3, 4]
 
 const WhyFlowra = ({ title, description, ListingComponent, specifications }) => {
     return (
-        <div className='px-20 my-20'>
+        <div className='w-11/12 mx-auto sm:px-4 lg:px-7 my-20'>
             <div className='flex flex-col items-center gap-6'>
-                <h2 className='text-primary text-5xl font-semiBold max-w-[600px] text-center'>
+                <h2 className='text-primary sm:text-5xl text-3xl font-semiBold max-w-[600px] text-center'>
                     { title }
                 </h2>
                 <p className='text-center text-xl text-gray-700 lg:max-w-5xl tracking-wide leading-9'>
